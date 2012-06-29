0fbf11c85f7127e0e6dbd692ce502753bdf7662b
Guoshun.Wu <Guoshun.Wu@alcatel-sbell.com.cn>
2012-06-29 13:36:58 +0800

    add Hibernate hbm files

---
+++ src/main/java/com/alcatel_lucent/dms/test/HibernateTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.alcatel_lucent.dms.test;
+
+import com.alcatel_lucent.dms.model.HibernateSessionFactory;
+
+/**
+ * @author Guoshun.Wu
+ * Date: 2012-06-28
+ *
+ */
+public class HibernateTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		HibernateSessionFactory.getConfiguration();
+	}
+}
