efbbc27b4cb333abcc634b3461f6824eb49d2d70
Guoshun.Wu <Guoshun.Wu@alcatel-sbell.com.cn>
2012-07-05 09:21:34 +0800

    adjust project structure

--- src/test/java/com/alcatel_lucent/dms/test/TestSpringContext.java
+++ src/test/java/com/alcatel_lucent/dms/test/TestSpringContext.java
@@ -3,15 +3,15 @@ package com.alcatel_lucent.dms.test;
 import org.logicalcobwebs.proxool.ProxoolFacade;
 
 import com.alcatel_lucent.dms.SpringContext;
-import com.alcatel_lucent.dms.service.DictionaryService;
+import com.alcatel_lucent.dms.service.DaoService;
 
 public class TestSpringContext {
 
 	public static void main(String[] args) throws Exception {
-		SpringContext.getService(DictionaryService.class);
+		SpringContext.getService(DaoService.class);
 //		String temp="{'name': 123, 'name2':'value2'}";
 //		Map map = (Map)JSONObject.fromObject(temp);
 //		System.out.println(map);
-		ProxoolFacade.shutdown(0);
+		ProxoolFacade.shutdown(1);
 	}
 }
