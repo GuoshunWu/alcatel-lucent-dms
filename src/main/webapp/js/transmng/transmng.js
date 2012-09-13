1ed9935d9c80ce4b5584580a7bdbefcbd93f40c4
Guoshun Wu <Guoshun.Wu@alcatel-sbell.com.cn>
2012-09-13 19:41:02 +0800

    requirejs test

--- src/main/webapp/js/transmng/transmng.js
+++ src/main/webapp/js/transmng/transmng.js
@@ -7,26 +7,40 @@
  */
 
 requirejs.config({
+    //By default load any module IDs from js/lib
+    baseUrl:'js/lib',
+    path:{
+        taskmng:'../taskmng',
+    },
     shim:{
-        'backbone':{
+        'jquery-ui-1.8.22.custom.min':{
             //These script dependencies should be loaded before loading
             //backbone.js
-            deps:['underscore', 'jquery'],
+            deps:['jquery'],
             //Once loaded, use the global 'Backbone' as the
             //module value.
-            exports:'Backbone'
-        },
-        'foo':{
-            deps:['bar'],
-            //A function can be used to generate the exported value.
-            //"this" for the function will be the global object.
-            //The dependencies will be passed in as function arguments.
-            exports:function (bar) {
-                //Using a function allows you to call noConflict for libraries
-                //that support it. However, be aware that plugins for those
-                //libraries may still want a global.
-                return this.Foo.noConflict();
-            }
+            exports:'jqueryui'
         }
+//        'foo':{
+//            deps:['bar'],
+//            //A function can be used to generate the exported value.
+//            //"this" for the function will be the global object.
+//            //The dependencies will be passed in as function arguments.
+//            exports:function (bar) {
+//                //Using a function allows you to call noConflict for libraries
+//                //that support it. However, be aware that plugins for those
+//                //libraries may still want a global.
+//                return this.Foo.noConflict();
+//            }
+//        }
     }
+});
+
+requirejs({},['jquery-ui-1.8.22.custom.min'],function(jqui){
+    $("#testDialog").dialog({
+        autoOpen:true,
+        buttons:{
+            'OK':function(){}
+        }
+    });
 });
