5c8936b096fb5731cba77a3aafd4b7001e3ffc0b
Allan Yang <allan.yang@alcatel-lucent.com>
2012-11-22 15:12:33 +0800

    add LogoutAction

---
+++ src/main/java/com/alcatel_lucent/dms/action/login/LogoutAction.java
@@ -0,0 +1,36 @@
+package com.alcatel_lucent.dms.action.login;
+
+import java.util.Map;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.alcatel_lucent.dms.UserContext;
+import com.alcatel_lucent.dms.action.BaseAction;
+
+@SuppressWarnings("serial")
+public class LogoutAction extends BaseAction implements SessionAware {
+	
+    private Map<String, Object> session;
+
+    @Override
+	@Action(results = {
+            @Result(name = SUCCESS, type = "redirect", location = "/login.jsp")
+    })
+	public String execute() {
+    	UserContext uc = (UserContext) session.get(UserContext.SESSION_USER_CONTEXT);
+    	if (uc != null) {
+    		log.info("User " + uc.getUser().getLoginName() + " logged out");
+    		session.remove(UserContext.SESSION_USER_CONTEXT);
+    	}
+		return SUCCESS;
+	}
+
+    @Override
+    public void setSession(Map<String, Object> session) {
+        this.session = session;
+    }
+
+
+}
