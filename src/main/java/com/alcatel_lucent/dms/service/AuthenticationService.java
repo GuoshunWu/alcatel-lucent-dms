ca5be30215bcaee8271723ad8b5bb07fbf9e5dde
Allan Yang <allan.yang@alcatel-lucent.com>
2012-11-28 14:48:13 +0800

    add authentication token support (not used yet)

--- src/main/java/com/alcatel_lucent/dms/service/AuthenticationService.java
+++ src/main/java/com/alcatel_lucent/dms/service/AuthenticationService.java
@@ -11,5 +11,21 @@ public interface AuthenticationService {
 	 * @return user object, null if login failed
 	 */
 	User login(String username, String password);
+	
+	/**
+	 * Perform login action and generate a temporary secure token if succeeded.
+	 * @param username CSL
+	 * @param password CIP
+	 * @return secure token, null if login failed
+	 */
+	public String secureLogin1(String username, String password);
+	
+	/**
+	 * Validate secure token and return corresponding user info.
+	 * The secure token will be invalidated once used.
+	 * @param token secure token
+	 * @return User object if token is validated, null if the token is not valid
+	 */
+	public User secureLogin2(String token);
 
 }
