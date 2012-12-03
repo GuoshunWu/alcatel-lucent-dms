9d693f9865ac02f4164a2e8822e49ec57e832831
Allan Yang <allan.yang@alcatel-lucent.com>
2012-12-03 18:38:48 +0800

    update labels

--- src/main/webapp/js/lib/jquery.blockUI.js
+++ src/main/webapp/js/lib/jquery.blockUI.js
@@ -83,7 +83,7 @@
 		$.blockUI.defaults = {
 			// message displayed when blocking (use null for no message)
 //			message:  '<h1>Please wait...</h1>',
-            message: '<h1><img src="images/busy.gif" /> Just a moment...</h1>',
+            message: '<h1><img src="images/busy.gif" />&nbsp;Please wait...</h1>',
 
 			title: null,	  // title string; only used when theme == true
 			draggable: true,  // only used when theme == true (requires jquery-ui.js to be loaded)
