c97bd4e85e5edd97a4febb3de06c6750e5af2efe
Guoshun Wu <Guoshun Wu@localhost>
2013-01-29 12:21:46 +0800

    update block ui.

--- src/main/webapp/js/lib/jquery.blockUI.js
+++ src/main/webapp/js/lib/jquery.blockUI.js
@@ -88,7 +88,8 @@
         // override these in your code to change the default behavior and style
         $.blockUI.defaults = {
             // message displayed when blocking (use null for no message)
-            message:'<h1>Please wait...</h1>',
+//			message:  '<h1>Please wait...</h1>',
+            message: '<h1><img src="images/busy.gif" />&nbsp;Please wait...</h1>',
 
             title:null, // title string; only used when theme == true
             draggable:true, // only used when theme == true (requires jquery-ui.js to be loaded)
