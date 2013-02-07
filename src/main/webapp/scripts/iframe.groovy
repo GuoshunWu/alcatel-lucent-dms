aa9ef914d795ec57922d1000fec9053913650e73
Guoshun Wu <Guoshun Wu@localhost>
2013-02-07 15:39:27 +0800

    update iframe reverse ajax.

--- src/main/webapp/scripts/iframe.groovy
+++ src/main/webapp/scripts/iframe.groovy
@@ -1,20 +1,32 @@
+import com.alcatel_lucent.dms.service.Worker
 import groovy.json.JsonBuilder
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import java.util.concurrent.BlockingQueue
+import java.util.concurrent.LinkedBlockingQueue
+
+Logger log = LoggerFactory.getLogger(this.getClass())
 
 response.contentType = 'text/html'
+JsonBuilder jsonBuilder = new JsonBuilder()
 
-String buildHtml(builder) {
+String buildHtml(event, builder = jsonBuilder) {
     builder {
         stamp System.currentTimeMillis()
-        name 'Hello'
-        value 'World'
+        msg event
     }
-    "<script type:'text/javascript'>parent.${params.callback}(${builder.toPrettyString()});</script>"
+    "<script>parent.${params.callback}(${builder.toPrettyString()});</script>"
 }
 
-def builder = new JsonBuilder()
+BlockingQueue<String> events = new LinkedBlockingQueue<>()
+new Thread(new Worker(events, Integer.parseInt(params.speed), Integer.parseInt(params.freq)), "Worker_${session.id}").start()
+fout = new PrintWriter(out, true)
+log.info "params: ${params}."
 
-while (true) {
-    Thread.sleep(1000)
-    println buildHtml(builder)
-    out.flush()
+while (event = events.take()) {
+    String toClient = buildHtml(event, jsonBuilder)
+    log.info("To client info: ${toClient}")
+    fout.println(toClient)
+    if (event.startsWith('done')) break
 }
