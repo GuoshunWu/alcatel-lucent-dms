76bbeddd48e0e0355ede8d734545ec5690e9eb43
Allan Yang <allan.yang@alcatel-lucent.com>
2013-02-28 10:03:48 +0800

    split transactions when deliver dictionaries to improve performance

--- src/main/java/com/alcatel_lucent/dms/action/app/DeliverDictAction.java
+++ src/main/java/com/alcatel_lucent/dms/action/app/DeliverDictAction.java
@@ -1,15 +1,20 @@
 package com.alcatel_lucent.dms.action.app;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.apache.struts2.convention.annotation.Result;
+import org.hibernate.Transaction;
 
 import com.alcatel_lucent.dms.BusinessException;
 import com.alcatel_lucent.dms.BusinessWarning;
 import com.alcatel_lucent.dms.Constants;
 import com.alcatel_lucent.dms.action.JSONAction;
 import com.alcatel_lucent.dms.model.Dictionary;
+import com.alcatel_lucent.dms.model.DictionaryLanguage;
 import com.alcatel_lucent.dms.service.DeliveringDictPool;
 import com.alcatel_lucent.dms.service.DeliveryReport;
 import com.alcatel_lucent.dms.service.DictionaryService;
@@ -30,7 +35,7 @@ public class DeliverDictAction extends JSONAction {
 	protected String performAction() throws Exception {
 		try {
 			Collection<Dictionary> dictList = deliveringDictPool.getDictionaries(handler);
-			report = dictionaryService.importDictionaries(app, dictList, Constants.DELIVERY_MODE);
+			report = importDictionaries(app, dictList, Constants.DELIVERY_MODE);
 			setMessage(report.toHTML());
 		} catch (BusinessException e) {
 			setMessage(e.toString());
@@ -38,6 +43,25 @@ public class DeliverDictAction extends JSONAction {
 		}
 		return SUCCESS;
 	}
+
+    private DeliveryReport importDictionaries(Long appId, Collection<Dictionary> dictList, int mode) throws BusinessException {
+        DeliveryReport report = new DeliveryReport();
+        Map<String, Collection<BusinessWarning>> warningMap = new TreeMap<String, Collection<BusinessWarning>>();
+        for (Dictionary dict : dictList) {
+            Map<String, String> langCharset = new HashMap<String, String>();
+            if (dict.getDictLanguages() != null) {
+                for (DictionaryLanguage dl : dict.getDictLanguages()) {
+                    langCharset.put(dl.getLanguageCode(), dl.getCharset().getName());
+                }
+            }
+            Collection<BusinessWarning> warnings = new ArrayList<BusinessWarning>();
+            dictionaryService.importDictionary(appId, dict, dict.getVersion(), mode, null, langCharset, warnings, report);
+            warningMap.put(dict.getName(), warnings);
+        }
+        report.setWarningMap(warningMap);
+        log.info(report.toString());
+        return report;
+    }
 
 	public String getHandler() {
 		return handler;
