f458ea10528cbb8b62696ecaad66e221eeed5867
Allan Yang <allan.yang@alcatel-lucent.com>
2013-04-11 17:26:07 +0800

    add UserREST

---
+++ src/main/java/com/alcatel_lucent/dms/rest/UserREST.java
@@ -0,0 +1,60 @@
+package com.alcatel_lucent.dms.rest;
+
+import java.util.Collection;
+import java.util.Map;
+
+import javax.ws.rs.Path;
+
+import org.springframework.stereotype.Component;
+
+import com.alcatel_lucent.dms.model.User;
+
+/**
+ * User REST service.
+ * URL: /rest/users
+ *   
+ * Sort parameters:
+ *   sidx		(optional) sort by, default is "loginName"
+ *   sord		(optional) order, default is "ASC"
+ *   
+ * Format parameters:
+ *   format		(optional) format of result string, possible values: "json|grid|tree", default is "json"
+ *   prop		(required) properties to be retrieved
+ *   			for json: prop={<prop1>,<prop2>,...} where each <prop> can be nested, 
+ *   					e.g. <prop2>=<prop_name>{<sub_prop1>,<sub_prop2>}
+ *   			for grid: prop=<property_name_for_column1>,<property_name_for_column2>,...
+ *   			for tree: prop=<property_name_for_id>,<property_name_for_name>
+ *   idprop		(optional) property name for id, for grid only
+ *   The result is not paged, that means "rows" and "page" parameter will not be supported.
+ *   		
+ * @author allany
+ *
+ */@Path("users")
+@Component("userREST")
+@SuppressWarnings("unchecked")
+public class UserREST extends BaseREST{
+
+	@Override
+	String doGetOrPost(Map<String, String> requestMap) throws Exception {
+		String hql = "from User";
+		String countHql = "select count(*) from User";
+    	String sidx = requestMap.get("sidx");
+    	String sord = requestMap.get("sord");
+    	if (sidx == null || sidx.trim().isEmpty()) {
+    		sidx = "loginName";
+    	}
+    	if (sord == null) {
+    		sord = "ASC";
+    	}
+    	hql += " order by " + sidx + " " + sord;
+		Collection<User> result = retrieve(hql, null, countHql, null, requestMap);
+    	requestMap.put("idprop", "loginName");
+		return toJSON(result, requestMap);
+	}
+
+	@Override
+	Class getEntityClass() {
+		return User.class;
+	}
+
+}
