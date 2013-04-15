2aff3abe1d35b22276b4d341d68370e7cdab4145
Allan Yang <allan.yang@alcatel-lucent.com>
2013-04-15 14:41:02 +0800

    add UserAction and UserService

---
+++ src/main/java/com/alcatel_lucent/dms/service/UserService.java
@@ -0,0 +1,25 @@
+package com.alcatel_lucent.dms.service;
+
+import com.alcatel_lucent.dms.BusinessException;
+import com.alcatel_lucent.dms.model.User;
+
+public interface UserService {
+	/**
+	 * Create a user.
+	 * @param user
+	 * @return
+	 */
+	User createUser(User user) throws BusinessException;
+	
+	/**
+	 * Update a user.
+	 * @param loginName login name of the user
+	 * @param name user name, null if no change
+	 * @param email email, null if no change
+	 * @param role user role, null if no change
+	 * @param status user status, null if no change
+	 * @return
+	 */
+	User updateUser(String loginName, String name, String email, Integer role, Integer status) throws BusinessException;
+	
+}
