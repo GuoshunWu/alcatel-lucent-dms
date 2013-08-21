22eacd28810b8989fac26a534ae3435a88d62673
Guoshun Wu <Guoshun Wu@localhost>
2013-08-21 11:58:33 +0800

    Add dirty flag to Glossary indicating whether it's a new glossary. Apply glossaries on dirty glossaries only.

--- src/main/java/com/alcatel_lucent/dms/service/GlossaryServiceImpl.java
+++ src/main/java/com/alcatel_lucent/dms/service/GlossaryServiceImpl.java
@@ -3,14 +3,17 @@ package com.alcatel_lucent.dms.service;
 import com.alcatel_lucent.dms.BusinessException;
 import com.alcatel_lucent.dms.UserContext;
 import com.alcatel_lucent.dms.model.*;
-import com.alcatel_lucent.dms.model.Dictionary;
 import com.alcatel_lucent.dms.util.Util;
+import org.apache.commons.collections.ClosureUtils;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.sql.Timestamp;
-import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -23,7 +26,7 @@ public class GlossaryServiceImpl implements GlossaryService {
 
     private DaoService dao;
     private Collection<Glossary> glossaries;
-    
+
     @Autowired
     private TextService textService;
 
@@ -46,7 +49,7 @@ public class GlossaryServiceImpl implements GlossaryService {
         if (language != null) {
             throw new BusinessException(BusinessException.GLOSSARY_ALREADY_EXISTS, text);
         }
-        User user= UserContext.getInstance().getUser();
+        User user = UserContext.getInstance().getUser();
         Glossary glossary = new Glossary(text, user);
         glossary = (Glossary) dao.create(glossary);
 
@@ -68,7 +71,6 @@ public class GlossaryServiceImpl implements GlossaryService {
             dao.delete(Glossary.class, id);
             glossaries.remove(glossary);
             glossary = createGlossary(text);
-            glossaries.add(glossary);
         }
         return glossary;
     }
@@ -98,35 +100,37 @@ public class GlossaryServiceImpl implements GlossaryService {
      */
     @Override
     public void consistentGlossaries() {
-        Collection<Glossary> glossaries = getAllGlossaries();
+        Collection<Glossary> glossaries = dao.retrieve("from Glossary where dirty = true");
 
-        String hSQL = "from Label where id in (select l.id from Label l, Glossary g where upper(l.reference) like concat( '%', upper(g.text), '%'))";
+        String hSQL = "from Label where id in (select l.id from Label l, Glossary g where g.dirty=true and upper(l.reference) like concat( '%', upper(g.text), '%'))";
         Collection<Label> labels = dao.retrieve(hSQL);
         Context ctxExclusion = textService.getContextByExpression(Context.EXCLUSION, null);
         for (Label label : labels) {
             label.setReference(Util.consistentGlossaries(label.getReference(), glossaries));
             if (isGlossary(label.getReference())) {
-            	label.setContext(ctxExclusion);
+                label.setContext(ctxExclusion);
             }
         }
 
-        hSQL = "from Text where id in (select t.id from Text t, Glossary g where upper(t.reference) like concat( '%', upper(g.text), '%'))";
+        hSQL = "from Text where id in (select t.id from Text t, Glossary g where g.dirty=true and upper(t.reference) like concat( '%', upper(g.text), '%'))";
         Collection<Text> texts = dao.retrieve(hSQL);
         for (Text text : texts) {
             text.setReference(Util.consistentGlossaries(text.getReference(), glossaries));
         }
 
-        hSQL = "from Translation where id in (select t.id from Translation t, Glossary g where upper(t.translation) like concat( '%', upper(g.text), '%'))";
+        hSQL = "from Translation where id in (select t.id from Translation t, Glossary g where g.dirty=true and upper(t.translation) like concat( '%', upper(g.text), '%'))";
         Collection<Translation> translations = dao.retrieve(hSQL);
         for (Translation translation : translations) {
             translation.setTranslation(Util.consistentGlossaries(translation.getTranslation(), glossaries));
         }
 
-        hSQL = "from LabelTranslation where id in (select lt.id from LabelTranslation lt, Glossary g where upper(lt.origTranslation) like concat( '%', upper(g.text), '%'))";
+        hSQL = "from LabelTranslation where id in (select lt.id from LabelTranslation lt, Glossary g where g.dirty=true and upper(lt.origTranslation) like concat( '%', upper(g.text), '%'))";
         Collection<LabelTranslation> lbTranslations = dao.retrieve(hSQL);
         for (LabelTranslation lbTranslation : lbTranslations) {
             lbTranslation.setOrigTranslation(Util.consistentGlossaries(lbTranslation.getOrigTranslation(), glossaries));
         }
+        // update dirty glossaries
+        CollectionUtils.forAllDo(glossaries, ClosureUtils.invokerClosure("setDirty", new Class[]{Boolean.TYPE}, new Object[]{false}));
 
     }
 
@@ -184,24 +188,25 @@ public class GlossaryServiceImpl implements GlossaryService {
     public void consistentGlossariesInLabelRef(Label label) {
         label.setReference(getConsistentGlossariesText(label.getReference()));
         if (isGlossary(label.getReference())) {
-        	label.setContext(textService.getContextByExpression(Context.EXCLUSION, null));
+            label.setContext(textService.getContextByExpression(Context.EXCLUSION, null));
         }
     }
 
     /**
      * Check if a string is in glossary list
+     *
      * @param s
      * @return
      */
     private boolean isGlossary(String s) {
-    	s = s.trim();
-		for (Glossary g : glossaries) {
-			if (g.getText().equals(s)) return true;
-		}
-		return false;
-	}
+        s = s.trim();
+        for (Glossary g : glossaries) {
+            if (g.getText().equals(s)) return true;
+        }
+        return false;
+    }
 
-	@Override
+    @Override
     public String getConsistentGlossariesText(String text) {
         return Util.consistentGlossaries(text, glossaries);
     }
@@ -210,6 +215,7 @@ public class GlossaryServiceImpl implements GlossaryService {
     private Glossary findGlossaryByText(String text) {
         return (Glossary) dao.retrieve(Glossary.class, text);
     }
+
 
     @Override
     public Collection<Glossary> getAllGlossaries() {
