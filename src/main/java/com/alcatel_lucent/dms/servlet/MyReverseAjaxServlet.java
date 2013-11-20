a746f86f100e33bd32db379fe7a64b236888c3ed
Guoshun Wu <Guoshun Wu@localhost>
2013-11-20 21:44:30 +0800

    resolve tomcat deploy permgen OOME issue

--- src/main/java/com/alcatel_lucent/dms/servlet/MyReverseAjaxServlet.java
+++ src/main/java/com/alcatel_lucent/dms/servlet/MyReverseAjaxServlet.java
@@ -6,13 +6,11 @@ import org.slf4j.LoggerFactory;
 
 import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.text.DateFormat;
 import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
