49dd64d77290b1ee790f7b2340a030e3bf9f2f79
Guoshun Wu <Guoshun Wu@localhost>
2013-12-13 11:53:23 +0800

    update Label exclusion

--- src/main/java/com/alcatel_lucent/dms/service/PatternPair.java
+++ src/main/java/com/alcatel_lucent/dms/service/PatternPair.java
@@ -6,14 +6,23 @@ import java.util.regex.Pattern;
  * Created by guoshunw on 13-12-11.
  */
 public class PatternPair {
-
-    PatternPair(Pattern pattern, String replacement) {
+    PatternPair(Pattern pattern, String replacement, String glossary) {
         this.pattern = pattern;
         this.replacement = replacement;
+        this.glossary = glossary;
     }
 
     private Pattern pattern;
     private String replacement;
+    private String glossary;
+
+    public PatternPair(String glossary) {
+        this.glossary = glossary;
+    }
+
+    public String getGlossary() {
+        return glossary;
+    }
 
     public Pattern getPattern() {
         return pattern;
