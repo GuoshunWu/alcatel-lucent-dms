b75e156e26d87839577a5aa45e59607f2309657c
Guoshun Wu <Guoshun Wu@localhost>
2013-12-23 17:54:20 +0800

    correct spelling error in release note update java rhino test case

--- src/test/groovy/com/alcatel_lucent/dms/service/parser/ParserTest.groovy
+++ src/test/groovy/com/alcatel_lucent/dms/service/parser/ParserTest.groovy
@@ -3,12 +3,15 @@ package com.alcatel_lucent.dms.service.parser
 import org.apache.commons.io.FileUtils
 import org.junit.*
 import org.junit.runner.RunWith
-import org.mozilla.javascript.Context
-import org.mozilla.javascript.Scriptable
-import org.mozilla.javascript.tools.shell.Global
+
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.transaction.TransactionConfiguration
+
+
+
+import org.mozilla.javascript.*
+import javax.script.*
 
 /**
  * Created by IntelliJ IDEA.
@@ -42,44 +45,90 @@ public class ParserTest {
     void tearDown() throws Exception {
     }
 
-    class TestClass{
-        String name
-        @Override
-        void finalize(){
-            println "\"${name}\"going to be cleaned."
-        }
-    }
-
     @Test
-    void testThreadLocal() {
-        println "=" * 100
-        TestClass t1 =new TestClass(name: "T1")
-        t1 = null
+    void testRhinoJS() throws Exception {
+
+        String filePath = "${System.getenv('NODE_PATH')}/coffee-script/lib/extra/coffee-script.js"
+//        filePath = "${System.getenv('NODE_PATH')}/coffee-script/lib/coffee-script/coffee-script.js"
+
+        String rJSPath = "${System.getenv('NODE_PATH')}/requirejs/bin/r_rhino.js"
+
+
+        String rJSPathStr = FileUtils.readFileToString(new File(rJSPath), "UTF-8")
+        String jsFileStr = FileUtils.readFileToString(new File(filePath), "UTF-8")
+
+        // Creates and enters a Context. The Context stores information
+        // about the execution environment of a script.
+        Context ctx = Context.enter()
+        try {
+            /**
+             * see: https://github.com/jashkenas/coffee-script/wiki/Using-CS-with-Java-Rhino
+             * there is a 64k byte code limit in Java for compiled code, and as both the full/minimised coffee-script.js and the individual files break this when compiled,
+             * you have to go down the interpreted javascript route, which means optimisation level -1 (ie its slower than it could be).
+             *
+             * the Rhino engine has no require facility, but the library RingoJS is a way to get some CommonsJS facilities
+             * */
+            ctx.setOptimizationLevel(-1); // Without this, Rhino hits a 64K bytecode limit and fails
+
+            final Scriptable globalScope = ctx.initStandardObjects()
+            // Add a global variable "out" that is a JavaScript reflection of System.out
+            Object jsOut = Context.javaToJS(System.out, globalScope)
+            ScriptableObject.putProperty(globalScope, "out", jsOut)
+//            String returnStr =ctx.evaluateString(globalScope, rJSPathStr, "require.js", 0, null)
+//            println returnStr
+//            return
+            ctx.evaluateString(globalScope, jsFileStr, "coffee-script.js", 1, null)
+
+            ScriptableObject coffeeScript = globalScope.get("CoffeeScript", globalScope)
+            String coffeeJSTest ="""
+(console=
+  log:(obj)->out.println(obj)
+  info:(obj)->out.println(obj)
+)unless console
+
+console.log 'This is my coffee script!'
+"""
+            //Coffee script compiled java scripts
+            Object result = ((Function)coffeeScript.get("compile")).call ctx, globalScope, coffeeScript, [coffeeJSTest] as Object[]
+            println "Compiled coffee script(js code)".center(100, '=')
+            println ctx.toString(result)
+
+            // Run coffee script directly
+            ((Function)coffeeScript.get("run")).call ctx, globalScope, coffeeScript, [coffeeJSTest] as Object[]
+
+        } finally {
+            ctx.exit()
+        }
     }
 
 //    @Test
-    void testParse() throws Exception {
-
-        String js = """
-        function testFun(p1, p2){
-            console.log("p1="+p1+", p2="+p2);
-        }
-       """
-        String filePath = "${System.getenv('NODE_PATH')}/coffee-script/lib/coffee-script/coffee-script.js"
+    void testJAVA6JS(){
+        String filePath = "${System.getenv('NODE_PATH')}/coffee-script/lib/extra/coffee-script.js"
         String jsFileStr = FileUtils.readFileToString(new File(filePath), "UTF-8")
 
-        Context ctx = Context.enter()
-        ctx.setOptimizationLevel(-1); // Without this, Rhino hits a 64K bytecode limit and fails
+        // create a script engine manager
+        ScriptEngineManager factory = new ScriptEngineManager();
+        // create JavaScript engine
+        ScriptEngine engine = factory.getEngineByName("JavaScript");
 
-        final Scriptable globalScope = ctx.initStandardObjects()
-        // Add some global function in the global scope see: http://stackoverflow.com/questions/12399462/rhino-print-function
-        Global global = new Global(ctx)
-        ctx.evaluateString(global, jsFileStr, "coffee-script.js", 0, null)
+        // evaluate JavaScript code from given file - specified by first argument
+        engine.eval(jsFileStr);
 
-        ctx.evaluateString(global, "print('Where is the dog?');var a='This is my test;'", "Test", 1, null)
-        globalScope.put("b", globalScope, "My test here")
-        ctx.evaluateString(global, "print('a=' + a+', b='+b);", "Test", 1, null)
-        ctx.exit()
+        // get script object on which we want to call the method
+        Object coffeeScript = engine.get("CoffeeScript")
+        Invocable inv = (Invocable) engine
+
+        String coffeeJSTest ="""
+(console=
+  log:(obj)->out.println(obj)
+  info:(obj)->out.println(obj)
+)unless console
+
+console.log 'This is my coffee script!'
+"""
+
+        inv.invokeMethod(coffeeScript, "run", coffeeJSTest)
+
 
     }
 }
