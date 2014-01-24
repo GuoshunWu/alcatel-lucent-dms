4201a23d5b9cbbb7942542127f23a7c5ea373fe1
Guoshun Wu <Guoshun Wu@localhost>
2014-01-24 22:33:38 +0800

    implement preference reference delete

--- src/main/java/com/alcatel_lucent/dms/model/PreferredTranslation.java
+++ src/main/java/com/alcatel_lucent/dms/model/PreferredTranslation.java
@@ -1,5 +1,9 @@
 package com.alcatel_lucent.dms.model;
 
+import org.hibernate.annotations.ForeignKey;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import javax.persistence.*;
 import java.util.Date;
 
