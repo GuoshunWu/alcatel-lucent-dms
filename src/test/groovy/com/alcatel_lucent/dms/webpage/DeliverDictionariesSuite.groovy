21d5aeb5a29017efb2cafdc8e3712b541e7f893f
Guoshun Wu <wgsmasm@hotmail.com>
2014-04-19 15:04:45 +0800

    refine test cases

---
+++ src/test/groovy/com/alcatel_lucent/dms/webpage/DeliverDictionariesSuite.groovy
@@ -0,0 +1,25 @@
+package com.alcatel_lucent.dms.webpage
+
+import junit.framework.Test
+import junit.framework.TestSuite
+import org.junit.AfterClass
+import org.junit.BeforeClass
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+/**
+ * Created by Administrator on 2014/4/19 0019.
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses([TestImportDictionary.class])
+class DeliverDictionariesSuite{
+
+    @BeforeClass public static void setUpClass() {
+        // Common initialization done once for Test1 + Test2
+    }
+    @AfterClass public static void tearDownClass() {
+        // Common cleanup for all tests
+    }
+
+}
